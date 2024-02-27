3bc19cd838852dde5a2386a14d3819503ffb1e72
Sv3ks <snorre@silkjaer.dk>
2024-02-27 13:15:46 +0100

    lexer

--- interpreter/lexer.py
+++ interpreter/lexer.py
@@ -1,70 +1,110 @@
 import re
 
-def cleanText(text):
-	return text.removeprefix(' ').removesuffix(' ')
+def clean_str(string):
+	return string.removeprefix(' ').removesuffix(' ')
 
-def tokenize(content):
-	def first_sort(string):
-		chars = list(string)
-		result = []
-		temp_str = ''
-		quoute_count = 0
-		for char in chars:
-			# Quote Stuff
-			if char == '"':
-				quoute_count += 1
-			if quoute_count % 2 == 0:
-				in_quotes = False
-			else:
-				in_quotes = True
-
-
-			# Split Paranthesis
-			if char == '(' and not in_quotes:
-				result.append(cleanText(temp_str))
-				temp_str = ''
-			elif char == ')' and not in_quotes:
-				result.append(first_sort(temp_str))
-				temp_str = ''
-			else:
-				temp_str += char
-
-		result.append(cleanText(temp_str))
-		return result
+def split_par(string):
+	chars = list(string)
+	result = []
+	temp_str = ''
+	quoute_count = 0
+	for char in chars:
+		# Quote Stuff
+		if char == '"':
+			quoute_count += 1
+		if quoute_count % 2 == 0:
+			in_quotes = False
+		else:
+			in_quotes = True
 
 
-
-	lines = content.split('\n')
-	for line in lines:
-		# First Sorting (Splitting Parenthesis)
-		tokens = first_sort(line)
+		# Split Paranthesis
+		if (char == '(' or char == ' ') and not in_quotes:
+			result.append(clean_str(temp_str))
+			temp_str = ''
+		elif char == ')' and not in_quotes:
+			result.append(split_par(temp_str))
+			temp_str = ''
+		else:
+			temp_str += char
 	
-		# Second Sorting (Sorting Data Types)
-		items = []
-		for token in tokens:
-			# Strings
-			if token == '':
-				continue
-			elif token[0] == '"':
-				if token[-1] == '"':
-					items.append(('string', token))
-				else:
-					# ERROR
-					print('Error... :(')
-			# Functions
-			elif re.match(r'[.a-zA-Z]+', token):
-				items.append(('symbol',token))
-			# Expressions
-			elif token in '+-/*':
-				items.append(('expression',token))
-			# Number
-			elif re.match(r'[.0-9]+',token):
-				items.append(('number',token))
+	result.append(clean_str(temp_str))
+	return result
+
+def split(string):
+	chars = list(string)
+	result = []
+	temp_str = ''
+	quoute_count = 0
+	for char in chars:
+		# Quote Stuff
+		if char == '"':
+			quoute_count += 1
+		if quoute_count % 2 == 0:
+			in_quotes = False
+		else:
+			in_quotes = True
 
 
-		print(items)
-		print(tokens)
+		# Split Paranthesis
+		if char == '(' and not in_quotes:
+			result.append(clean_str(temp_str))
+			temp_str = ''
+		elif char == ')' and not in_quotes:
+			result.append(split_par(temp_str))
+			temp_str = ''
+		else:
+			temp_str += char
 
-if __name__ == '__main__':
-	contents = open('../examples/hello-world.pk').read()
-	tokenize(contents)
+	result.append(clean_str(temp_str))
+	return result
+
+def tokenize(field):
+	if field[0] == '"':
+		if field[-1] == '"':
+			return ('string', field.removeprefix('"').removesuffix('"'))
+		else:
+			# ERROR
+			return ('Error... :(')
+	# Functions
+	elif re.match(r'[.a-zA-Z]+', field):
+		return ('symbol',field)
+	# Expressions
+	elif field in '+-/*':
+		return ('expression',field)
+	# Number
+	elif re.match(r'[.0-9]+',field):
+		return ('number',field)
+	# None :(
+	else:
+		return ('Error... :(')
+
+def tokenize_arr(arr):
+	tokens = []
+	for field in arr:
+		#print(field)
+		# Check If Token Is String Or Array
+		if str(field) == field:
+			# Make sure field is not empty
+			if not field == '':
+				tokens.append(tokenize(field))
+		# It's an Array
+		else:
+			tokens.append(tokenize_arr(field))
+	return tokens
+
+
+
+def lexer(content):
+	lines = content.split('\n')
+	result = []
+	for line in lines:
+		# First Sorting (Splitting Into Fields)
+		fields = split(line)
+
+		# Second Sorting (Tokenizing Fields)
+		tokens = tokenize_arr(fields)
+
+		result.append(tokens)
+
+	return result
