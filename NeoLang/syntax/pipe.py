5b255de6e49163fd9556f228c266470b9b763bda
Sv3ks <snorre@silkjaer.dk>
2024-03-09 19:14:20 +0100

    Update pipe.py

--- NeoLang/syntax/pipe.py
+++ NeoLang/syntax/pipe.py
@@ -1,12 +1,16 @@
  #* Pipe between packages and interpreter
 
-from os import listdir
+from os import listdir, path
+from pathlib import Path
 from syntax.classes.package import Package
 
 def get_packages():
 	packages = []
-	dir = './NeoScript/syntax/packages'
+
+	 #* Path thingy gets the path of the directory this file is in.
+	 #? For more info, read https://stackoverflow.com/a/3430395/15400585 - it helped me out a lot!
+	dir = f'{path.dirname(path.abspath(__file__))}/packages'
 	for package in listdir(dir):
 		packages.append(Package(f'{dir}/{package}'))
 	
-	return packages
+	return package
