9889c4fdc97884bf66fa725590cc107177ead25d
Sv3ks <snorre@silkjaer.dk>
2024-03-12 13:19:54 +0100

    Update parser.py

--- NeoLang/interpreter/parser.py
+++ NeoLang/interpreter/parser.py
@@ -2,8 +2,14 @@ class Parser:
 	def __init__(self, packages = [], tokens = []) -> None:
 		self.packages = packages
 		self.tokens = tokens
+	def expression(self,token):
+		'Parse expression and return output of expression'
+	def args(self,token):
+		'Parse arguments and return result of arguments'
+		'example: 1+"hello" => "1hello"'
 	def effect(self, token):
-		''
+		'Parse and run effect'
+		print(str(token))
 
 	def parse(self):
 		for token in self.tokens:
