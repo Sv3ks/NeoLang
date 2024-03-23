0f65724f320a210ea739ec6a6fe814ba04e36d69
Sv3ks <snorre@silkjaer.dk>
2024-03-23 17:55:35 +0100

    📃 | Package List Command

--- NeoLang/commands/__init__.py
+++ NeoLang/commands/__init__.py
@@ -1,7 +1,9 @@
 from NeoLang.commands.run import run
 from NeoLang.commands.version import version
+from NeoLang.commands.packages import packages
 
 COMMANDS = {
 	'run': run,
 	'version': version,
+	'packages': packages,
 }
