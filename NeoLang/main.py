d94e338dd10e47c0a3cb3ff58008eb022e8c7cad
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2024-03-24 23:13:04 +0100

    👋 | Hello Message

--- NeoLang/main.py
+++ NeoLang/main.py
@@ -8,6 +8,10 @@ from NeoLang.commands import COMMANDS
 def Main():
 	args = sys.argv
 	args.pop(0)
+	if args == []:
+		print('Hello from NeoLang!')
+		return
+	
 	for command in COMMANDS:
 		if args[0].lower() != command.lower(): continue
 		args.pop(0)
