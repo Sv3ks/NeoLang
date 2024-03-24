ecd8d812edb01fed6f5c1c1e59f29760c54d89a7
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2024-03-24 21:14:35 +0100

    🗝️ | GetTokens Command

--- NeoLang/commands/__init__.py
+++ NeoLang/commands/__init__.py
@@ -1,9 +1,11 @@
 from NeoLang.commands.run import run
 from NeoLang.commands.version import version
 from NeoLang.commands.packages import packages
+from NeoLang.commands.gettokens import gettokens
 
 COMMANDS = {
 	'run': run,
 	'version': version,
 	'packages': packages,
+	'gettokens': gettokens,
 }
